210430134510f7f93afaf0d9eab79857b80dd266
venkata-hari <xprogrammer.me@gmail.com>
2024-10-17 23:16:10 +0530

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; 
+const port = 10000; 
 
 server.use(middlewares);
 server.use(router);
